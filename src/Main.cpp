a531fe14bf6f9aaca8f5970b7a523e39ee4cffe5
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-06-06 09:43:23 +1200

    Added Identifiers and Hash Table

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <memory.h>
 
+#include <type_traits>
+
 template<typename T>
 struct Array {
 	T* Data;
@@ -27,7 +29,7 @@ struct Array {
 
 template<typename T>
 Array<T> Array_Create(u64 capacity = 0) {
-	Array<T> result = (Array<T>) {
+	Array<T> result = {
 		.Data = capacity > 0 ? cast(T*) malloc(capacity * sizeof(T)) : nullptr,
 		.Length = 0,
 		.Capacity = capacity,
@@ -36,7 +38,7 @@ Array<T> Array_Create(u64 capacity = 0) {
 }
 
 template<typename T>
-void Array_Destroy(Array<T> array) {
+void Array_Destroy(Array<T>& array) {
 	free(array.Data);
 }
 
@@ -53,6 +55,71 @@ void Array_Push(Array<T>& array, const T& value) {
 	}
 
 	memcpy(&array.Data[array.Length++], &value, sizeof(T));
+}
+
+template<typename K>
+u64 HashTable_Hash(const K& key) {
+	static_assert(!std::is_same<void, void>(), "Please create a hash function for this type");
+	return 0;
+}
+
+template<typename K, typename V, u64 S = 10>
+struct HashTable {
+	struct Entry {
+		K Key;
+		V Value;
+	};
+
+	Array<Entry> Buckets[S];
+};
+
+template<typename K, typename V, u64 S = 10>
+HashTable<K, V, S> HashTable_Create() {
+	HashTable<K, V, S> result = {};
+	for (u64 i = 0; i < S; i++) {
+		result.Buckets[i] = Array_Create<typename HashTable<K, V, S>::Entry>();
+	}
+	return result;
+}
+
+template<typename K, typename V, u64 S>
+void HashTable_Destroy(HashTable<K, V, S>& table) {
+	for (u64 i = 0; i < S; i++) {
+		Array_Destroy(table.Buckets[i]);
+	}
+}
+
+template<typename K, typename V, u64 S>
+b8 HashTable_Add(HashTable<K, V, S>& table, const K& key, const V& value) {
+	u64 hash = HashTable_Hash(key) % S;
+	Array<typename HashTable<K, V, S>::Entry>& bucket = table.Buckets[hash];
+
+	for (u64 i = 0; i < bucket.Length; i++) {
+		if (bucket[i].Key == key) {
+			return false;
+		}
+	}
+
+	Array_Push(bucket, {
+		.Key = key,
+		.Value = value,
+	});
+
+	return true;
+}
+
+template<typename K, typename V, u64 S>
+V* HashTable_Get(HashTable<K, V, S>& table, const K& key) {
+	u64 hash = HashTable_Hash(key) % S;
+	Array<typename HashTable<K, V, S>::Entry>& bucket = table.Buckets[hash];
+
+	for (u64 i = 0; i < bucket.Length; i++) {
+		if (bucket[i].Key == key) {
+			return &bucket[i].Value;
+		}
+	}
+
+	return nullptr;
 }
 
 struct String {
@@ -72,10 +139,66 @@ struct String {
 		#endif
 		return this->Data[index];
 	}
+
+	const u8& operator[](u64 index) const {
+		#if !defined(NO_ARRAY_BOUNDS_CHECK)
+			ASSERT(index < this->Length);
+		#endif
+		return this->Data[index];
+	}
+
+	const u8& operator[](s64 index) const {
+		#if !defined(NO_ARRAY_BOUNDS_CHECK)
+			ASSERT(index >= 0 && cast(u64) index < this->Length);
+		#endif
+		return this->Data[index];
+	}
 };
 
 #define StringFromLiteral(s) (String) { .Data = cast(u8*) s, .Length = sizeof(s) - 1 }
 #define StringPrintfFormat(s) cast(u32) s.Length, s.Data
+
+b8 operator==(const String& a, const String& b) {
+	if (a.Length != b.Length) {
+		return false;
+	}
+
+	if (a.Data == b.Data) {
+		return true;
+	}
+
+	return memcmp(a.Data, b.Data, a.Length * sizeof(*String::Data)) == 0;
+}
+b8 operator!=(const String& a, const String& b) {
+	return !(a == b);
+}
+
+static Array<String> InternedStrings = Array_Create<String>();
+String InternString(String oldString) {
+	for (u64 i = 0; i < InternedStrings.Length; i++) {
+		if (InternedStrings[i] == oldString) {
+			return InternedStrings[i];
+		}
+	}
+
+	String newString = {};
+	newString.Data = cast(u8*) malloc(oldString.Length * sizeof(*String::Data));
+	memcpy(newString.Data, oldString.Data, oldString.Length * sizeof(*String::Data));
+	newString.Length = oldString.Length;
+
+	Array_Push(InternedStrings, newString);
+	return newString;
+}
+
+template<>
+u64 HashTable_Hash<String>(const String& string) {
+	u64 hash = 0;
+	for (u64 i = 0; i < string.Length; i++) {
+		hash += string[i];
+		hash *= string[i];
+	}
+	return hash;
+}
 
 enum TokenKind : u8 {
 	TokenKind_EndOfInput,
@@ -248,13 +371,58 @@ Token* Lexer_NextToken(Lexer* lexer) {
 				u64 base = 10;
 				u64 intValue = 0;
 
+				if (Lexer_GetCurrentChar(lexer) == '0') {
+					Lexer_NextChar(lexer);
+					length++;
+					switch (Lexer_GetCurrentChar(lexer)) {
+						case 'B': case 'b': {
+							base = 2;
+							Lexer_NextChar(lexer);
+							length++;
+						} break;
+
+						case 'X': case 'x': {
+							base = 16;
+							Lexer_NextChar(lexer);
+							length++;
+						} break;
+
+						default: {
+							base = 8;
+						} break;
+					}
+				}
+
 				while (true) {
 					switch (Lexer_GetCurrentChar(lexer)) {
 						case '0': case '1': case '2': case '3': case '4':
-						case '5': case '6': case '7': case '8': case '9': {
+						case '5': case '6': case '7': case '8': case '9':
+						case 'a': case 'b': case 'c': case 'd': case 'e':
+						case 'f': case 'g': case 'h': case 'i': case 'j':
+						case 'k': case 'l': case 'm': case 'n': case 'o':
+						case 'p': case 'q': case 'r': case 's': case 't':
+						case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+						case 'A': case 'B': case 'C': case 'D': case 'E':
+						case 'F': case 'G': case 'H': case 'I': case 'J':
+						case 'K': case 'L': case 'M': case 'N': case 'O':
+						case 'P': case 'Q': case 'R': case 'S': case 'T':
+						case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z': {
+							u8 chr = Lexer_NextChar(lexer);
+							u64 value = 0;
+
+							ASSERT(base <= 36);
+							if (chr >= 'a' && chr <= 'z') {
+								value = cast(u64) chr - 'a' + 10;
+							} else if (chr >= 'A' && chr <= 'Z') {
+								value = cast(u64) chr - 'A' + 10;
+							} else if (chr >= '0' && chr <= '9') {
+								value = cast(u64) chr - '0';
+							} else {
+								ASSERT(false);
+							}
+
 							intValue *= base;
-							ASSERT(base <= 10);
-							intValue += Lexer_NextChar(lexer) - '0';
+							intValue += value;
 							length++;
 						} continue;
 
@@ -276,6 +444,52 @@ Token* Lexer_NextToken(Lexer* lexer) {
 				return Token_CreateInteger(TokenKind_Integer, start, startLine, startColumn, length, intValue);
 			} break;
 
+			case 'a': case 'b': case 'c': case 'd': case 'e':
+			case 'f': case 'g': case 'h': case 'i': case 'j':
+			case 'k': case 'l': case 'm': case 'n': case 'o':
+			case 'p': case 'q': case 'r': case 's': case 't':
+			case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+			case 'A': case 'B': case 'C': case 'D': case 'E':
+			case 'F': case 'G': case 'H': case 'I': case 'J':
+			case 'K': case 'L': case 'M': case 'N': case 'O':
+			case 'P': case 'Q': case 'R': case 'S': case 'T':
+			case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+			case '_': {
+				Array<u8> chars = Array_Create<u8>();
+				defer(Array_Destroy(chars));
+
+				while (true) {
+					switch (Lexer_GetCurrentChar(lexer)) {
+						case '0': case '1': case '2': case '3': case '4':
+						case '5': case '6': case '7': case '8': case '9':
+						case 'a': case 'b': case 'c': case 'd': case 'e':
+						case 'f': case 'g': case 'h': case 'i': case 'j':
+						case 'k': case 'l': case 'm': case 'n': case 'o':
+						case 'p': case 'q': case 'r': case 's': case 't':
+						case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+						case 'A': case 'B': case 'C': case 'D': case 'E':
+						case 'F': case 'G': case 'H': case 'I': case 'J':
+						case 'K': case 'L': case 'M': case 'N': case 'O':
+						case 'P': case 'Q': case 'R': case 'S': case 'T':
+						case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+						case '_': {
+							Array_Push(chars, Lexer_NextChar(lexer));
+							length++;
+						} continue;
+
+						default: {
+						} break;
+					}
+					break;
+				}
+
+				String identifier = InternString({
+					.Data = chars.Data,
+					.Length = chars.Length,
+				});
+				return Token_CreateString(TokenKind_Identifier, start, startLine, startColumn, length, identifier);
+			} break;
+
 			default: {
 				ASSERT(false);
 			} break;
@@ -287,6 +501,7 @@ Token* Lexer_NextToken(Lexer* lexer) {
 }
 
 enum AstKind : u8 {
+	AstKind_Identifier,
 	AstKind_Integer,
 	AstKind_UnaryOperator,
 	AstKind_BinaryOperator,
@@ -298,44 +513,60 @@ struct Ast {
 	AstKind Kind;
 };
 
-struct Ast_Integer : Ast {
+struct Ast_Expression : Ast {
+};
+
+struct Ast_Identifier : Ast_Expression {
+	Token* IdentifierToken;
+};
+
+Ast_Identifier* Ast_Identifier_Create(Token* identifierToken) {
+	Ast_Identifier* ast = cast(Ast_Identifier*) calloc(1, sizeof(*ast));
+	*ast = {
+		{ { .Kind = AstKind_Identifier } },
+		.IdentifierToken = identifierToken,
+	};
+	return ast;
+}
+
+struct Ast_Integer : Ast_Expression {
 	Token* IntToken;
 };
 
 Ast_Integer* Ast_Integer_Create(Token* intToken) {
 	Ast_Integer* ast = cast(Ast_Integer*) calloc(1, sizeof(*ast));
 	*ast = {
-		{ .Kind = AstKind_Integer },
+		{ { .Kind = AstKind_Integer } },
 		.IntToken = intToken,
 	};
 	return ast;
 }
 
-struct Ast_UnaryOperator : Ast {
+struct Ast_UnaryOperator : Ast_Expression {
 	Token* Operator;
-	Ast* Operand;
+	Ast_Expression* Operand;
 };
 
-Ast_UnaryOperator* Ast_UnaryOperator_Create(Token* _operator, Ast* operand) {
+Ast_UnaryOperator* Ast_UnaryOperator_Create(Token* _operator, Ast_Expression* operand) {
 	Ast_UnaryOperator* ast = cast(Ast_UnaryOperator*) calloc(1, sizeof(*ast));
 	*ast = {
-		{.Kind = AstKind_UnaryOperator },
+		{ { .Kind = AstKind_UnaryOperator } },
 		.Operator = _operator,
 		.Operand = operand,
 	};
 	return ast;
 }
 
-struct Ast_BinaryOperator : Ast {
-	Ast* Left;
+struct Ast_BinaryOperator : Ast_Expression {
+	Ast_Expression* Left;
 	Token* Operator;
-	Ast* Right;
+	Ast_Expression* Right;
 };
 
-Ast_BinaryOperator* Ast_BinaryOperator_Create(Ast* left, Token* _operator, Ast* right) {
+Ast_BinaryOperator* Ast_BinaryOperator_Create(Ast_Expression* left, Token* _operator, Ast_Expression* right) {
 	Ast_BinaryOperator* ast = cast(Ast_BinaryOperator*) calloc(1, sizeof(*ast));
 	*ast = {
-		{.Kind = AstKind_BinaryOperator },
+		{ { .Kind = AstKind_BinaryOperator } },
 		.Left = left,
 		.Operator = _operator,
 		.Right = right,
@@ -436,20 +667,24 @@ u64 Parser_GetBinaryOperatorPresedence(TokenKind kind) {
 	return 0;
 }
 
-Ast* Parser_ParseExpression(Parser* parser) {
-	Ast* Parser_ParseBinaryExpression(Parser * parser, u64 parentPresedence);
+Ast_Expression* Parser_ParseExpression(Parser* parser) {
+	Ast_Expression* Parser_ParseBinaryExpression(Parser * parser, u64 parentPresedence);
 	return Parser_ParseBinaryExpression(parser, 0);
 }
 
-Ast* Parser_ParsePrimaryExpression(Parser* parser) {
+Ast_Expression* Parser_ParsePrimaryExpression(Parser* parser) {
 	switch (Parser_GetCurrentToken(parser)->Kind) {
 		case TokenKind_Integer: {
 			return Ast_Integer_Create(Parser_NextToken(parser));
 		} break;
 
+		case TokenKind_Identifier: {
+			return Ast_Identifier_Create(Parser_NextToken(parser));
+		} break;
+
 		case TokenKind_OpenParentheses: {
 			Parser_NextToken(parser);
-			Ast* expression = Parser_ParseExpression(parser);
+			Ast_Expression* expression = Parser_ParseExpression(parser);
 			ASSERT(Parser_NextToken(parser)->Kind == TokenKind_CloseParentheses);
 			return expression;
 		} break;
@@ -463,13 +698,13 @@ Ast* Parser_ParsePrimaryExpression(Parser* parser) {
 	return nullptr;
 }
 
-Ast* Parser_ParseBinaryExpression(Parser* parser, u64 parentPresedence) {
-	Ast* left;
+Ast_Expression* Parser_ParseBinaryExpression(Parser* parser, u64 parentPresedence) {
+	Ast_Expression* left;
 
 	u64 unaryOperatorPrecedence = Parser_GetUnaryOperatorPresedence(Parser_GetCurrentToken(parser)->Kind);
 	if (unaryOperatorPrecedence != 0 && unaryOperatorPrecedence > parentPresedence) {
 		Token* _operator = Parser_NextToken(parser);
-		Ast* operand = Parser_ParseBinaryExpression(parser, unaryOperatorPrecedence);
+		Ast_Expression* operand = Parser_ParseBinaryExpression(parser, unaryOperatorPrecedence);
 		left = Ast_UnaryOperator_Create(_operator, operand);
 	}
 	else {
@@ -483,7 +718,7 @@ Ast* Parser_ParseBinaryExpression(Parser* parser, u64 parentPresedence) {
 		}
 
 		Token* _operator = Parser_NextToken(parser);
-		Ast* right = Parser_ParseBinaryExpression(parser, presedence);
+		Ast_Expression* right = Parser_ParseBinaryExpression(parser, presedence);
 		left = Ast_BinaryOperator_Create(left, _operator, right);
 	}
 
@@ -491,7 +726,7 @@ Ast* Parser_ParseBinaryExpression(Parser* parser, u64 parentPresedence) {
 }
 
 int main(int argc, char** argv) {
-	String source = StringFromLiteral("-1 + 2 * (3 + +4)");
+	String source = StringFromLiteral("value + hello * idk12345 / var1");
 
 #if 0
 	Lexer* lexer = Lexer_Create(source);
@@ -522,20 +757,26 @@ int main(int argc, char** argv) {
 	Parser* parser = Parser_Create(source);
 	defer(Parser_Destroy(parser));
 
-	Ast* ast = Parser_ParseExpression(parser);
-	void PrintAst(Ast* ast);
+	Ast_Expression* ast = Parser_ParseExpression(parser);
+#if 0
+	void PrintAst(Ast_Expression* ast);
 	PrintAst(ast);
+#endif
 
 	return 0;
 }
 
-void PrintAst(Ast* ast) {
+void PrintAst(Ast_Expression* ast) {
 	ASSERT(ast);
 
 	switch (ast->Kind) {
+		case AstKind_Identifier: {
+			Ast_Identifier* identifier = cast(Ast_Identifier*) ast;
+			printf("%.*s", StringPrintfFormat(identifier->IdentifierToken->StringValue));
+		} break;
+
 		case AstKind_Integer: {
 			Ast_Integer* integer = cast(Ast_Integer*) ast;
-			ASSERT(integer->IntToken->Kind == TokenKind_Integer);
 			printf("%llu", integer->IntToken->IntValue);
 		} break;
 
